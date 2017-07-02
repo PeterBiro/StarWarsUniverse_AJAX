e38a7d985bcae5666ed1edfbbab82786092678ac
Péter Biró <biro.peter.hu@gmail.com>
2017-07-02 14:01:58 +0200

    residents modal
    Residenets modal is working, gets all data from swapi.

--- static/main.js
+++ static/main.js
@@ -10,7 +10,47 @@ function loadAnotherPage(){
     
 }
 
+function showResidents(){
+    var headerKeys = ["name", "height", "mass", "skin_color", "hair_color", "eye_color", "birth_year", "gender"];
+
+    // making of residents(and their datas) list
+
+    var residentsUrlList = this.dataset.residentsUrl;
+    residentsUrlList = residentsUrlList.replace("]", "").replace("[", "").replace(/'/g, "").split(",");
+    document.getElementById("tbody").innerHTML = ""; 
+    for(let i =0; i < residentsUrlList.length; i++) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {             
+                var tbody = document.getElementById("tbody");
+                var row, cell;
+                row = tbody.insertRow();
+                var responseJSON = JSON.parse(this.response);
+                for (var n = 0; n < headerKeys.length; n++) {
+                    cell = row.insertCell();
+                    if(headerKeys[n] == "height"){
+                        var height = parseInt(responseJSON[headerKeys[n]]);
+                        cell.innerHTML = (height/100).toString(); 
+                    } else {
+                        cell.innerHTML = responseJSON[headerKeys[n]];
+                    }
+
+                }
+                
+            }
+        };
+        xhttp.open("GET", residentsUrlList[i], true);
+        xhttp.send();
+    }
+
+}
+
+
 var list = document.getElementsByClassName("page-btn");
-for(let i =0; i < list.length; i++) {
+for(let i = 0; i < list.length; i++) {
     list[i].addEventListener("click", loadAnotherPage);
+}
+var resBtnList = document.getElementsByClassName("resident-btn");
+for(let i = 0; i < resBtnList.length; i++) {
+    resBtnList[i].addEventListener("click", showResidents);
 }
